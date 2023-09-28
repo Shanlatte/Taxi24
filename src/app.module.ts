c0e97134e54aace6b443a8e5984f136e10ce5b36
Shanlatte <pedroeshanlatte@gmail.com>
2023-09-28 00:35:38 -0400

    feat: Add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.PORT),
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
