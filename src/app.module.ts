f6ca518edd4213f0cca3e07c49a26432f8e53429
Shanlatte <pedroeshanlatte@gmail.com>
2023-10-01 02:08:25 -0400

    feat: Add migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,33 +2,28 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { PersonsModule } from './persons/persons.module';
-import { Person } from './persons/entities/person.entity';
 import { DriversModule } from './drivers/drivers.module';
 import { LocationsModule } from './locations/locations.module';
-import { Driver } from './drivers/entities/driver.entity';
-import { Location } from './locations/entities/location.entity';
 import { PassengersModule } from './passengers/passengers.module';
-import { Passenger } from './passengers/entities/passenger.entity';
 import { RidesModule } from './rides/rides.module';
-import { Ride } from './rides/entities/ride.entity';
 import { InvoicesModule } from './invoices/invoices.module';
-import { Invoice } from './invoices/entities/invoice.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig, DatabaseConfig } from './config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.PORT),
-      password: process.env.DATABASE_PASSWORD,
-      username: process.env.DATABASE_USERNAME,
-      database: process.env.DATABASE_NAME,
-      entities: [Person, Driver, Location, Passenger, Ride, Invoice],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DatabaseConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService]
     }),
     PersonsModule,
     DriversModule,
---
+++ src/config/database.config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from "@nestjs/config";
+
+const config = {
+    type: 'postgres',
+    host: `${process.env.DATABASE_HOST}`,
+    port: `${parseInt(process.env.DATABASE_PORT, 10)}`,
+    username: `${process.env.DATABASE_USERNAME}`,
+    password: `${process.env.DATABASE_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
+    entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+    migrations: [`${__dirname}/../migrations/*{.ts,.js}`],
+    synchronize: `${process.env.NODE_ENV}` === 'development',
+    logging: false,
+    migrationsTableName: 'migrations'
+}
+
+export default registerAs('database', () => config)
